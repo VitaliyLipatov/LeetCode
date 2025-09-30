e059ab4450668a7cd49830fa8162aeb95e3c72e1
vitaliy <vitaliy.lipatov1107@gmail.com>
2025-09-30 23:10:04 +0500

    https://leetcode.com/problems/merge-two-sorted-lists

---
+++ src/leetcode/MergeTwoSortedLists.java
@@ -0,0 +1,63 @@
+package leetcode;
+
+public class MergeTwoSortedLists {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(4);
+        l1.next = l2;
+        l2.next = l3;
+        ListNode l4 = new ListNode(1);
+        ListNode l5 = new ListNode(3);
+        ListNode l6 = new ListNode(4);
+        l4.next = l5;
+        l5.next = l6;
+        System.out.println(mergeTwoLists(l1, l4));
+        System.out.println(mergeTwoLists(null, null));
+        ListNode l0 = new ListNode(0);
+        System.out.println(mergeTwoLists(null, l0));
+    }
+
+    private static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode result = new ListNode();
+        ListNode current = result;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        current.next = (list1 != null) ? list1 : list2;
+
+        return result.next;
+    }
+
+    private static class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return STR."ListNode{val=\{val}, next=\{next}}";
+        }
+    }
+}
